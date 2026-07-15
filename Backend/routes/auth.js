327e0eff616c72e092bf7c7c31bada23a2b0fd85
Manish1678-sos <manishbhowmik1234@gmail.com>
2026-07-15 11:02:25 +0530

    feat: user registration with express-validator and schema fix

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,13 +1,35 @@
 const express=require('express');
-
-const router=express.Router();
 const User=require('../models/Users');
+const router=express.Router();
+
+const{body, validationResult}= require('express-validator');
 
 //Create a User using:POST "/api/auth/". Doesn't require Auth
-router.post('/',(req,res) =>{
-    console.log(req.body);
-    const user= new User(req.body);
-    user.save();
-    res.send("Hello Manish!")
-})
+router.post('/',
+    [body('name','Enter a valid name').isLength({min:3}),
+        
+        body('email','Enter a valid email').isEmail(),
+        body('password','Enter a valid password').isLength({min:5}),],
+        
+        (req,res) =>{
+            const errors = validationResult(req);
+            if(!errors.isEmpty()){
+                return res.status(400).json({errors:errors.array()});
+
+                        } 
+    User.create({
+        name:req.body.name,
+        password:req.body.password,
+        email:req.body.email
+    }).then(user=>res.json(user))
+    .catch(err=>{
+        console.log(err);
+        res.status(400).json({error:"Email already exists",message:err.message});
+    });
+    });
+
+                    
+    
+
+
 module.exports=router;
