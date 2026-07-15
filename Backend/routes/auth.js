df54f33b592fae50728696839e6b0b53647532a7
Manish1678-sos <manishbhowmik1234@gmail.com>
2026-07-15 09:54:21 +0530

    feat: setup auth route and mongodb connection

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,12 +1,13 @@
 const express=require('express');
-const router= express.Router();
 
-router.get('/',(req,res) =>{
-    const obj={
-        a:'thios',
-        number:34
+const router=express.Router();
+const User=require('../models/Users');
 
-    }
-    res.json(obj)
+//Create a User using:POST "/api/auth/". Doesn't require Auth
+router.post('/',(req,res) =>{
+    console.log(req.body);
+    const user= new User(req.body);
+    user.save();
+    res.send("Hello Manish!")
 })
 module.exports=router;
