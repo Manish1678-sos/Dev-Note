3b7e78a3990bd0fcc6e4f485caceecc2ce5e546e
Manish1678-sos <manishbhowmik1234@gmail.com>
2026-07-15 20:19:21 +0530

    auth route fixed and error handling tested

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -11,22 +11,35 @@ router.post('/',
         body('email','Enter a valid email').isEmail(),
         body('password','Enter a valid password').isLength({min:5}),],
         
-        (req,res) =>{
+        async(req,res) =>{
             const errors = validationResult(req);
             if(!errors.isEmpty()){
                 return res.status(400).json({errors:errors.array()});
 
-                        } 
-    User.create({
+            }
+       try{ let user =await User.findOne({email:req.body.email});
+        
+        if(user){
+            return res.status(400).json({error:"Sorry a user with this email already exists"});
+        }
+     user =await User.create({
         name:req.body.name,
         password:req.body.password,
         email:req.body.email
-    }).then(user=>res.json(user))
-    .catch(err=>{
-        console.log(err);
-        res.status(400).json({error:"Email already exists",message:err.message});
-    });
-    });
+    }) 
+    //.then(user=>res.json(user))
+    //.catch(err=>{
+       // console.log(err);
+        //res.status(400).json({error:"Email already exists",message:err.message});
+    res.json({message:"User created successfully"})
+    }
+catch(error)
+{
+    console.error(error.message);
+    res.status(500).send("Some error occured");
+}
+        });
+    //});
 
                     
     
