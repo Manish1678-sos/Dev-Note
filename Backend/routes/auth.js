0229674ad64e70a6b533b730d5cd6b1b1075f109
Manish1678-sos <manishbhowmik1234@gmail.com>
2026-07-15 21:14:15 +0530

    feat: implement JWT auth token generation and password hashing with bcrypt

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,6 +1,10 @@
 const express=require('express');
 const User=require('../models/Users');
 const router=express.Router();
+const bcrypt=require('bcryptjs');
+var jwt=require('jsonwebtoken');
+const JWT_SECRET="Manishisagoodboy";
+
 
 const{body, validationResult}= require('express-validator');
 
@@ -18,19 +22,30 @@ router.post('/',
 
             }
 //Check whether the User with this email exists already
-       try{ let user =await User.findOne({email:req.body.email});
+try{ let user =await User.findOne({email:req.body.email});
         
-        if(user){
-            return res.status(400).json({error:"Sorry a user with this email already exists"});
-        }
-    //Create a new User
+if(user){
+return res.status(400).json({error:"Sorry a user with this email already exists"});
+}
+const salt= await bcrypt.genSalt();
+
+const secPass=await bcrypt.hash(req.body.password, salt)
+
+//Create a new User
      user =await User.create({
         name:req.body.name,
-        password:req.body.password,
+        password:secPass,
         email:req.body.email
-    }) 
-   
-    res.json({message:"User created successfully"})
+    });
+    const data={
+        user:{
+            id:user.id
+
+        }
+    };
+const authToken=jwt.sign(data,JWT_SECRET);
+
+ res.json({authToken});
     }
 //Catch errors
 catch(error)
