d5b7c51b3beb93587e443382fa10068093b2db2b
Manish1678-sos <manishbhowmik1234@gmail.com>
2026-07-17 09:58:38 +0530

    fix: resolve route syntax error and implement delete note functionality

--- Backend/routes/Note.js
+++ Backend/routes/Note.js
@@ -39,5 +39,48 @@ res.json(saveNote)
     res.status(500).send("Internal server error");
     }
 })
+// ROUTE:3:-Update an existing note: Put "/api/Note/updatenote/:id".Login required
+router.put('/updatenote/:id',fetchUser,async(req,res)=>{
+    const{title,description,tag}=req.body;
+    //Create a newnote object
+    try{const newNote={};
+    if(title){newNote.title=title};
+        if(description){newNote.description=description};
+        if(tag){newNote.tag=tag};
+//Find the note to be updated and update it
+let note=await Note.findById(req.params.id);
+if(!note){res.status(404).send("Not found")};
+if(note.user.toString()!==req.user.id){
+    return res.status(401).send("Not allowed");
+}
+ note=await Note.findByIdAndUpdate(req.params.id,{$set:newNote},{retunDocument:"after"})
+res.json(note)
+    }
+    catch(error){
+         console.log(error.message);
+    res.status(500).send("Internal server error");
 
+    }
+
+})
+// ROUTE:3:-Delete an existing note: Delete "/api/Note/deleteenote/:id".Login required
+router.delete('/deletenote/:id',fetchUser,async(req,res)=>{
+    
+    
+try{
+//Find the note to be deletd and delete it
+let note=await Note.findById(req.params.id);
+if(!note){res.status(404).send("Not found")};
+//Allow deletion only if user owns this note
+if(note.user.toString()!==req.user.id){
+    return res.status(401).send("Not allowed");
+}
+ note=await Note.findByIdAndDelete(req.params.id,{retunDocument:"after"})
+res.json({Success: "Note has been deleted",note:note})
+    }
+    catch(error){
+         console.log(error.message);
+    res.status(500).send("Internal server error");
+    }
+});
 module.exports = router;
