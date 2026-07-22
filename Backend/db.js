8aac2c6ed1dd50649254e77c47faba29aa6cce76
Manish1678-sos <manishbhowmik1234@gmail.com>
2026-07-22 22:44:50 +0530

    Solved DB problem

--- Backend/db.js
+++ Backend/db.js
@@ -1,17 +1,15 @@
 const mongoose = require('mongoose');
-
-
 require('dotenv').config();
 
 
-const mongoURI = process.env.MONGO_URI;
+const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/devnote";
 
 const connectToMongo = async () => {
     try {
         await mongoose.connect(mongoURI);
         console.log("Connected to Mongo Successfully!");
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
+    } catch (err) {
+        console.error("Error connecting to MongoDB:", err);
     }
 }
 
